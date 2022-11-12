266c66c48119ff0c3f5c65cd4d5768af9a5ddef2
Eli Entelis <eli.entelis@hexagon.com>
2022-11-12 12:57:37 +0200

    add cloudword

---
+++ src/components/cloudWord.js
@@ -0,0 +1,47 @@
+import { TagCloud } from "react-tagcloud";
+import React, { useState, useEffect } from "react";
+
+const data = [
+  { value: "JavaScript", count: 38 },
+  { value: "React", count: 30 },
+  { value: "Nodejs", count: 28 },
+  { value: "Express.js", count: 25 },
+  { value: "HTML5", count: 33 },
+  { value: "MongoDB", count: 18 },
+  { value: "CSS3", count: 20 },
+];
+
+export const SimpleCloud = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5056/api/WordCloud")
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setIsLoaded(true);
+          setUsers(data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    console.log("ho" + users);
+    return (
+      <TagCloud
+        minSize={12}
+        maxSize={35}
+        tags={data}
+        onClick={(tag) => alert(`'${tag.value}' was selected!`)}
+      />
+    );
+  }
+};
